5f67b562482c301457500f137c38a70d2f2c39c7
Thiago Carvalho <thiagocarvalho.ads@gmail.com>
2019-10-02 10:34:51 -0300

    Criação de ambiente e estrutura do projeto

---
+++ README.md
@@ -0,0 +1,15 @@
+# 😃 Aircnc - Semana Omnistack 9 
+
+* Frontend ReactJS
+
+Aircnc é uma aplicação inspirada no app Airbnb pela galera da rocketseat
+
+---
+
+Dependencias:
+
+- ✔️ 
+- ✔️ 
+- ✔️ 
+
+---
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello Omnistack 9
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
