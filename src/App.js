93b1ded39ff590999961346dc04de4f3f1b6d7a6
Thiago Carvalho <thiagocarvalho.ads@gmail.com>
2019-10-03 00:34:53 -0300

    Criação tela login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg'
+
+import Routes from './routes';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Omnistack 9
-        </p>
-      </header>
+    <div className="container">
+      <img src={logo} alt="logo"/>
+      <div className="content">
+        <Routes />
+        
+      </div>
     </div>
   );
 }
